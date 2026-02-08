be10c69b8d5f2fc97b4bc568039607875b571ecc
Turja Roy <turja.roy@outlook.com>
2026-02-08 14:57:45 -0600

    Corrected plots and results

---
+++ shell_scripts/run_comps.sh
@@ -0,0 +1,39 @@
+python analyze_spectra.py compare \
+  spectra/IllustrisTNG/1P/1P_p1_0/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p2_0/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p7_0/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p8_0/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p9_0/camel_lya_spectra_snap_080_n10000.hdf5 \
+  --labels "p1_0,p2_0,p7_0,p8_0,p9_0"
+
+python analyze_spectra.py compare \
+  spectra/IllustrisTNG/1P/1P_p1_1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p2_1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p7_1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p8_1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p9_1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  --labels "p1_1,p2_1,p7_1,p8_1,p9_1"
+
+python analyze_spectra.py compare \
+  spectra/IllustrisTNG/1P/1P_p1_2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p2_2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p7_2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p8_2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p9_2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  --labels "p1_2,p2_2,p7_2,p8_2,p9_2"
+
+python analyze_spectra.py compare \
+  spectra/IllustrisTNG/1P/1P_p1_n1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p2_n1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p7_n1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p8_n1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p9_n1/camel_lya_spectra_snap_080_n10000.hdf5 \
+  --labels "p1_n1,p2_n1,p7_n1,p8_n1,p9_n1"
+
+python analyze_spectra.py compare \
+  spectra/IllustrisTNG/1P/1P_p1_n2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p2_n2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p7_n2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p8_n2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  spectra/IllustrisTNG/1P/1P_p9_n2/camel_lya_spectra_snap_080_n10000.hdf5 \
+  --labels "p1_n2,p2_n2,p7_n2,p8_n2,p9_n2"
